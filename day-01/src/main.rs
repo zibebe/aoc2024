bada4c9ca0244fa3e225db7d31bc8260079da778
Tobias Tschinkowitz <me@zibebe.net>
2024-12-02 19:52:24 +0100

    refactoring of day-01 part 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -2,19 +2,29 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("day-01/input.txt").expect("input.txt file is missing");
-    let mut left_ids = Vec::new();
-    let mut right_ids = Vec::new();
-    for (left, right) in input.split('\n').filter_map(|x| x.split_once("   ")) {
-        let left: i32 = left.parse().unwrap();
-        let right: i32 = right.parse().unwrap();
-        left_ids.push(left);
-        right_ids.push(right);
-    }
-    left_ids.sort();
-    right_ids.sort();
-    let mut total_distance = 0;
-    for (left, right) in left_ids.iter().zip(right_ids.iter()) {
-        total_distance += (left - right).abs();
-    }
-    println!("Answer: {}", total_distance);
+
+    // Create a Vector for the left and right elements and parse them to i32
+    let (mut left_ids, mut right_ids): (Vec<i32>, Vec<i32>) = input
+        .split('\n')
+        .filter_map(|x| x.split_once("   "))
+        .map(|(left, right)| {
+            let left: i32 = left.parse().unwrap();
+            let right: i32 = right.parse().unwrap();
+            (left, right)
+        })
+        .unzip();
+
+    // Sort them ascending
+    left_ids.sort_unstable();
+    right_ids.sort_unstable();
+
+    // Calculate the total distance, which is the difference between each value on the left and right
+    // summed up
+    let total_distance: i32 = left_ids
+        .iter()
+        .zip(right_ids.iter())
+        .map(|(left, right)| (left - right).abs())
+        .sum();
+
+    println!("Answer of Part 1: {}", total_distance);
 }
